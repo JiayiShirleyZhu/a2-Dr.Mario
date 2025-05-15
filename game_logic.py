2ebde04ef1b7194f4f53beac6b6d0ac1c2e54cba
Jiayi Zhu <jzhu42@uci.edu>
2025-05-14 20:23:40 -0700

    debug

--- game_logic.py
+++ game_logic.py
@@ -32,8 +32,8 @@ class GameState:
         self.faller = None  # The currently falling capsule
         self.half_capsules = []
         self.capsules = []
-        self.matching_ready = False  # If a matching needs to be cleared next
         self.game_over = False
+        self.matched_set = set()
 
     def initialize_field(self, rows: int, columns: int, setting: str, contents: list[str] = None) -> None:
         """
@@ -70,15 +70,12 @@ class GameState:
                         self.half_capsules.append(half_capsule)
 
             self.find_matching()
-            self.matching_ready = True
 
     def time_passed(self) -> None:
         """
         Updates the field for one time step.
         """
-        if self.matching_ready:
-            self.clear_matching()
-            self.matching_ready = False
+        self.clear_matching()
 
         self.apply_gravity()
 
@@ -88,7 +85,7 @@ class GameState:
             self.faller = None
 
         self.find_matching()
-        self.matching_ready = True
+
 
     def create_faller(self, color1: str, color2: str) -> None:
         """
@@ -162,16 +159,52 @@ class GameState:
         """
         if 0 <= row < self.rows and 0 <= col < self.columns and self.field[row][col] == ' ':
             self.field[row][col] = color.lower()
+    
+    def _get_hc_row(self, half_capsule: HalfCapsule) -> int:
+        """
+        Get the row of the half capsule
+
+        Args:
+            half_capsule: the half capsule I want to get row of
+
+        Returns:
+            row position of the half capsule
+        """
+        return half_capsule.row
+
+    def _get_capsule_row(self, capsule: tuple[HalfCapsule, HalfCapsule]) -> int:
+        """
+        Return the larger row number from the two half-capsules in the capsule.
+        This helps us apply gravity from bottom to top.
+
+        Args:
+            capsule: A tuple of two HalfCapsule objects
+
+        Returns:
+            The maximum row value
+        """
+        return max(capsule[0].row, capsule[1].row)
+
 
     def apply_gravity(self) -> None:
         """
         Moves all falling capsules or half capsules down if possible.
         """
-        for half_capsule in self.half_capsules:  # apply gravity on half capsule
+        half_capsules_sorted = sorted(self.half_capsules, key=self._get_hc_row, reverse=True)
+        capsules_sorted = sorted(self.capsules, key=self._get_capsule_row, reverse=True)
+        for half_capsule in half_capsules_sorted:  # apply gravity on half capsule
+            if half_capsule.state == 'frozen':
+                if half_capsule.row < self.rows - 1 and self.field[half_capsule.row + 1][half_capsule.col] == ' ':
+                    half_capsule.state = 'falling'
+
             if half_capsule.state == 'falling':
                 if half_capsule.delay:
                     half_capsule.delay = False
                     continue
+
+                elif (half_capsule.row + 1, half_capsule.col) in self.matched_set:
+                    continue
+                
                 if half_capsule.row < self.rows - 1 and self.field[half_capsule.row + 1][half_capsule.col] == ' ':
                     self.field[half_capsule.row + 1][half_capsule.col] = half_capsule.color
                     self.field[half_capsule.row][half_capsule.col] = ' '
@@ -179,12 +212,8 @@ class GameState:
 
                 if half_capsule.row == self.rows - 1 or self.field[half_capsule.row + 1][half_capsule.col] != ' ':
                     half_capsule.state = 'frozen'
-            
-            if half_capsule.state == 'frozen':
-                if half_capsule.row < self.rows - 1 and self.field[half_capsule.row + 1][half_capsule.col] == ' ':
-                    half_capsule.state = 'falling'
 
-        for capsule in self.capsules:  # apply gravity on capsule
+        for capsule in capsules_sorted:  # apply gravity on capsule
             if capsule[0].state == 'falling' and capsule[1].state == 'falling':
                 if capsule[0].orientation == 'horizontal':
                     if capsule[0].row < self.rows - 1 \
@@ -197,6 +226,33 @@ class GameState:
                         self.field[capsule[1].row][capsule[1].col] = ' '
                         capsule[1].row += 1
                 else:
+                    if capsule[0].row < self.rows - 1 \
+                        and self.field[capsule[0].row + 1][capsule[0].col] == ' ':
+                        # capsule[0] will always be the bottom left cell
+                        self.field[capsule[0].row + 1][capsule[0].col] = capsule[0].color
+                        self.field[capsule[1].row + 1][capsule[1].col] = capsule[1].color
+                        self.field[capsule[1].row][capsule[1].col] = ' '
+                        capsule[0].row += 1
+                        capsule[1].row += 1
+
+            elif capsule[0].state == 'frozen' and capsule[1].state == 'frozen':
+                if capsule[0].orientation == 'horizontal':
+                    if (capsule[0].row + 1, capsule[0].col) in self.matched_set or (capsule[1].row + 1, capsule[1].col) in self.matched_set:  # Avoid a capsule immediately fall into the space left by a just-cleared match.
+                        return
+                    
+                    if capsule[0].row < self.rows - 1 \
+                        and self.field[capsule[0].row + 1][capsule[0].col] == ' ' \
+                        and self.field[capsule[1].row + 1][capsule[1].col] == ' ':
+                        self.field[capsule[0].row + 1][capsule[0].col] = capsule[0].color
+                        self.field[capsule[0].row][capsule[0].col] = ' '
+                        capsule[0].row += 1
+                        self.field[capsule[1].row + 1][capsule[1].col] = capsule[1].color
+                        self.field[capsule[1].row][capsule[1].col] = ' '
+                        capsule[1].row += 1
+                else:
+                    if (capsule[0].row + 1, capsule[0].col) in self.matched_set or (capsule[1].row + 1, capsule[1].col) in self.matched_set:
+                        return
+                    
                     if capsule[0].row < self.rows - 1 \
                         and self.field[capsule[0].row + 1][capsule[0].col] == ' ':
                         # capsule[0] will always be the bottom left cell
@@ -251,6 +307,7 @@ class GameState:
                     self.field[r][c].upper() == self.field[r + 1][c].upper() == self.field[r + 2][c].upper() == self.field[r + 3][c].upper():
                     matched_set.update({(r, c), (r + 1, c), (r + 2, c), (r + 3, c)})
 
+        self.matched_set = matched_set
         return matched_set
 
     def clear_matching(self) -> None:
@@ -273,8 +330,8 @@ class GameState:
             elif (capsule[0].row, capsule[0].col) in matched_set:
                 remove_lst2.append((capsule[0], capsule[1]))
                 capsule[1].state = 'falling'
-                self.half_capsules.append(capsule[1])
                 capsule[1].delay = True
+                self.half_capsules.append(capsule[1])
             elif (capsule[1].row, capsule[1].col) in matched_set:
                 remove_lst2.append((capsule[0], capsule[1]))
                 capsule[0].state = 'falling'
